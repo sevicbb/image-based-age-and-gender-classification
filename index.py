29eeddcf39accffb1eea62cba2ea3fa503d606bf
Djordje Sevic <djordjesevic@gmail.com>
2021-07-08 00:11:54 +0200

    Fix packages

--- index.py
+++ index.py
@@ -1,8 +1,19 @@
 from data.loader import data_loader, frontal_data_loader
 
+# from sklearn.metrics import confusion_matrix
+
+import matplotlib.pyplot as plt
+
+def plot_confusion_matrix(matrix, title='Confusion matrix', cmap = plt.cm.Blues):
+    plt.imshow(matrix, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+
 def main():
     data_loader()
     frontal_data_loader()
+
+    plot_confusion_matrix(None)
 
 if __name__ == "__main__":
     main()
